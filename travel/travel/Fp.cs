15181c1854f0f136da39ea1c4003c6a06dd87b49
Nithu.kb <115065205+nithikb123@users.noreply.github.com>
2022-11-29 11:59:46 +0530

    Add files via upload

---
+++ travel/travel/Fp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace travel
+{
+    internal class Fp
+    {
+        public string name { get; set; }
+        public int Miles { get; set; }
+        private int _points;
+
+        public int Points
+        {
+            get
+            { return _points; }
+            set
+            {
+                _points = value;
+                if (Miles >= 10000 && Miles < 20000)
+                {
+                    _points = 10;
+                }
+
+                else if (Miles >= 20000 && Miles < 50000)
+                {
+                    _points = 20;
+                }
+
+                else if (Miles >= 50000 && Miles < 100000)
+                {
+                    _points = 30;
+                }
+
+                else
+                {
+                    _points = 50;
+                }
+
+            }
+        }
+    }
+}
---
+++ travel/travel/Program.cs
@@ -0,0 +1,11 @@
+﻿using travel;
+
+var p = new Fp();
+Console.WriteLine("enter name and miles travelled");
+Console.WriteLine("name:");
+p.name = Console.ReadLine();
+Console.WriteLine("miles:");
+p.Miles=int.Parse(Console.ReadLine());
+p.Points = 0;
+Console.WriteLine($"The frequent flyer point is {p.Points}");
+
