a76cf5ab6a9c613498dfebc10636cf264ffeb3a9
Nithu.kb <115065205+nithikb123@users.noreply.github.com>
2022-11-29 11:56:58 +0530

    Add files via upload

---
+++ Dob/Dob/Program.cs
@@ -0,0 +1,21 @@
+﻿DateTime Dob;
+Console.WriteLine("Enter dob of passenger");
+Dob = DateTime.Parse ((Console.ReadLine()));
+
+int age = (int)((DateTime.Now - Dob).TotalDays/365.242199);
+Console.WriteLine($" The age in years is {age}");
+double days = ((DateTime.Now - Dob).TotalDays);
+Console.WriteLine($"The age in days is {days}");
+double month = days / 30.41;
+Console.WriteLine($"The age in month is {month}");
+double hours = days * 24;
+Console.WriteLine($"The age in hours is {hours}");
+double minutes = hours * 60;
+Console.WriteLine($"The age in hours is {minutes}");
+double seconds = minutes * 60;
+Console.WriteLine($"The age in minutes is {seconds}");
+
+if(Dob.Year%4==0)
+{
+    Console.WriteLine("The person was born in leap year");
+}
